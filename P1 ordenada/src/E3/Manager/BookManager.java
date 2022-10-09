7d816ed2200f27d8cf5534fc674fee756a5d9f9d
LuisBallesteros <55761141+i82baarl@users.noreply.github.com>
2022-10-09 20:32:25 +0200

    Add files via upload

--- P1 ordenada/src/E3/Manager/BookManager.java
+++ P1 ordenada/src/E3/Manager/BookManager.java
@@ -1,5 +1,46 @@
 package E3.Manager;
 
+import java.util.*;
+
+import E2.Book.Book;
+
 public class BookManager {
+	
+	// Variables
+	
+	private static BookManager instance = null;
+	
+	private ArrayList<Book> books = new ArrayList<Book>();
+	
+	// Constructores
+	
+	public BookManager() {
+		
+	}
+	
+	// Observadores
+	
+	public static BookManager getInstance() {
+		
+		if( instance == null) {
+			instance = new BookManager();
+		}
+		return instance;
+	}
+	
+	public ArrayList<Book> getBooks(){
+		return books;
+	}
+	
+	// Modificadores
+	
+	// Metodos auxiliares
+	
+	public void addBook(Book book) {
+		
+		books.add(book);
+	}
+
+	
 	
 }
